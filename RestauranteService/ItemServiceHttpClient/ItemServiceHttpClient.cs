852ca5540f2e9e5f70985ed928fe21173a4b1de4
lucas souza <lucassousa@setic.ro.gov.br>
2022-09-01 20:03:17 -0400

    Adiconado HttpClient

---
+++ RestauranteService/ItemServiceHttpClient/ItemServiceHttpClient.cs
@@ -0,0 +1,25 @@
+﻿using RestauranteService.Dtos;
+using System.Text;
+using System.Text.Json;
+
+namespace RestauranteService.ItemServiceHttpClient
+{
+    public class ItemServiceHttpClient : IItemServiceHttpClient
+    {
+        private readonly HttpClient _client;
+        public ItemServiceHttpClient(HttpClient client)
+        {
+            _client = client;
+        }
+        public void EnviaRestauranteParaItemService(RestauranteReadDto restauranteReadDto)
+        {
+            var conteudoHttp = new StringContent
+                (
+                    JsonSerializer.Serialize(restauranteReadDto),
+                    Encoding.UTF8,
+                    "aplicatino/json"
+                );
+            
+        }
+    }
+}
