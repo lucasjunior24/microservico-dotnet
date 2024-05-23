96a23ed40030fcb7c11052320157471fce8e62cd
Souza <lucas11souza97@gmail.com>
2024-05-22 22:37:06 -0400

    feat: created Dockerfile

--- RestauranteService/obj/Debug/net6.0/RestauranteService.AssemblyInfo.cs
+++ RestauranteService/obj/Debug/net6.0/RestauranteService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestauranteService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852ecf07af8554d8ac609189fc9f7e601c27b8e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37215288cc1bbc9498a06c1c1f520f5199adb1df")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestauranteService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestauranteService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
